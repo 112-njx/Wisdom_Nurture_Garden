382fdb58b11bb87e144c2491ae17ecae9a6989c8
112-njx <1323407968@qq.com>
2025-10-06 14:18:18 +0800

    建立springboot依赖

---
+++ src/main/java/com/Wisdom_Nurture_Garden/demo/WisdomNurtureGardenApplication.java
@@ -0,0 +1,13 @@
+package com.Wisdom_Nurture_Garden.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisdomNurtureGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisdomNurtureGardenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Wisdom_Nurture_Garden/demo/WisdomNurtureGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wisdom_Nurture_Garden.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisdomNurtureGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
